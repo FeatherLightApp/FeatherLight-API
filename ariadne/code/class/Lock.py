e907496ad17c56253deb51d6a47284f0bc690906
Sean aye <sean.aye2@gmail.com>
2020-01-03 13:04:49 +0900

    fixed lock class

--- ariadne/code/class/Lock.py
+++ ariadne/code/class/Lock.py
@@ -1,22 +1,24 @@
 """Class for locking Redis instance"""
-from datetime.datetime import now
+from datetime import datetime
 
 class Lock:
-
+    """Class for locking redis db"""
     def __init__(self, redis, lock_key):
         self._redis = redis
         self._lock_key = lock_key
 
     async def obtain_lock(self):
-        timestamp = now()
-        set_result = await this._redis.setnx(this._lock_key, timestamp)
+    """obtain lock based on current time"""
+        utc_seconds = (datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds()
+        set_result = await self._redis.setnx(self._lock_key, utc_seconds)
         # fail if value already exists
         if not set_result:
             return False
         # expire lock after 5 mins as failsafe
-        await this._redis.expire(this._lock_key, 5 * 60)
+        await self._redis.expire(self._lock_key, 5 * 60)
         # lock success
         return True
 
     async def release_lock(self):
-        await this._redis.del(this._lock_key)
+    """release obtained lock"""
+        await self._redis.delete(self._lock_key)
