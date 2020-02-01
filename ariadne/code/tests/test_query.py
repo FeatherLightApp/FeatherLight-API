c51474b72a1c5036ef00804140d9ea2935e125db
Sean aye <sean.aye2@gmail.com>
2020-02-01 17:41:27 +0900

    add pytest

--- ariadne/code/tests/test_query.py
+++ ariadne/code/tests/test_query.py
@@ -28,7 +28,13 @@ def login(client, user):
             refresh
         }
     }
-    """
+    """ % (user[0], user[1])
+    res = client.post('/graphql/', json={'query': login_query})
+    assert res.status_code == 200
+    js res.json()
+    assert (access := js['data']['login']['access'])
+    assert (refress := js['data']['login']['refresh'])
+    return access, refresh
 
 
 def test_app():
@@ -36,6 +42,10 @@ def test_app():
         user1 = make_user(client)
         user2 = make_user(client)
         assert user1[0] != user2[0] and user1[1] != user2[1]
+        token1 = login(client, user1)
+        token2 = login(client, user2)
+
+
 
 
 
