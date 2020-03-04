bf50948cc62d5c9502c71cf792703ba4421cf961
Sean aye <sean.aye2@gmail.com>
2020-03-03 19:38:41 -0500

    update case sensitivity

--- ariadne/code/classes/__init__.py
+++ ariadne/code/classes/__init__.py
@@ -1,7 +1,7 @@
 """module for classes required by the application"""
-from .JWT import JWT
-from .Lock import Lock
-from .Paym import Paym
-from .User import User
-from .Context import Context
-from .Bitcoin import BitcoinClient
+from .jwt import JWT
+from .lock import Lock
+from .paym import Paym
+from .user import User
+from .context import Context
+from .bitcoin import BitcoinClient
