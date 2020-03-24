7d298db131845b4bdf1d73f8eef73286e58ef2ab
Sean Aye <hello@seanaye.ca>
2020-03-24 01:46:45 -0400

    add schema dir env var

--- ariadne_server/resolvers/schema.py
+++ ariadne_server/resolvers/schema.py
@@ -1,4 +1,5 @@
 """import and define schem execution"""
+import os
 from ariadne import (
     load_schema_from_path,
     make_executable_schema,
@@ -7,7 +8,7 @@ from ariadne import (
 from .types import TYPES as resolvers
 from .directives import AuthDirective, DatetimeDirective, RatelimitDirective
 
-TYPE_DEFS = load_schema_from_path('./schema/')
+TYPE_DEFS = load_schema_from_path(os.environ.get('SCHEMA_DIR'))
 
 SCHEMA = make_executable_schema(
     TYPE_DEFS,
