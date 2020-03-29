7576aeef876e7e9a2ee8c06a30c496d7488ddbbd
Sean Aye <hello@seanaye.ca>
2020-03-29 19:14:41 -0400

    change setup fn

--- ariadne_server/tests/fixtures/setup.py
+++ ariadne_server/tests/fixtures/setup.py
@@ -2,15 +2,13 @@ import pytest
 from context import GINO, REDIS, LND
 from resolvers.schema import SCHEMA
 
-@pytest.fixture(scope='session')
-async def setup_db():
-    print('initializing connections for testing')
+
+@pytest.fixture(autouse=True, scope='session')
+async def schema():
     await GINO.initialize()
     await REDIS.initialize()
     await LND.initialize()
-
-
-@pytest.fixture(autouse=True, scope='session')
-def schema():
-    return SCHEMA
+    yield SCHEMA
+    await GINO.destroy()
+    await REDIS.destroy()
     
