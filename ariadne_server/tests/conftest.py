89361d55ec35d8bf70aefa0dfbcf20617ef241b7
Sean Aye <hello@seanaye.ca>
2020-03-29 19:15:29 -0400

    fix import

--- ariadne_server/tests/conftest.py
+++ ariadne_server/tests/conftest.py
@@ -1,7 +1,7 @@
 """configuration file for pytest"""
 import pytest
 from tests.fixtures.context import context
-from tests.fixtures.setup import schema, setup_db
+from tests.fixtures.setup import schema
 
 #inject users list to use created users in multiple tests
 def pytest_configure():
