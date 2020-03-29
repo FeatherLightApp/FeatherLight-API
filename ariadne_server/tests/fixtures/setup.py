2d2340b4490dc3a7117a4b6b60825d8c64ff6eb1
Sean Aye <hello@seanaye.ca>
2020-03-29 18:01:31 -0400

    setup all connections

--- ariadne_server/tests/fixtures/setup.py
+++ ariadne_server/tests/fixtures/setup.py
@@ -1,10 +1,12 @@
 import pytest
-from context import GINO
+from context import GINO, REDIS, LND
 from resolvers.schema import SCHEMA
 
 @pytest.fixture(scope='session')
 async def setup_db():
     await GINO.initialize()
+    await REDIS.initialize()
+    await LND.initialize()
 
 
 @pytest.fixture(autouse=True, scope='session')
