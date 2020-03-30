48522686a6774c618888f260ae06bc6cc66221ed
Sean Aye <hello@seanaye.ca>
2020-03-29 22:21:30 -0400

    update env var setting

--- ariadne_server/tests/conftest.py
+++ ariadne_server/tests/conftest.py
@@ -1,4 +1,8 @@
 """configuration file for pytest"""
+import os
+
+os.environ['POSTGRES_DB'] = 'test'
+
 import pytest
 from tests.fixtures.context import context
 from tests.fixtures.setup import schema, event_loop
