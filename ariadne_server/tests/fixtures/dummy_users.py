9d9930ebfb94f902b06394b2274780d1eb3287a6
Sean Aye <hello@seanaye.ca>
2020-03-30 15:02:24 -0400

    fix create user fixtures

--- ariadne_server/tests/fixtures/dummy_users.py
+++ ariadne_server/tests/fixtures/dummy_users.py
@@ -1,3 +1,4 @@
+from time import time
 import pytest
 from classes.user import User
 from context import ARGON
@@ -8,7 +9,8 @@ async def dummy_user():
         id='de58dd1453a64fe4be9e',
         username='37ebb3ba1453f985e785',
         password_hash=ARGON.hash('055071c9b1c48946ceae'),
-        role='USER'
+        role='USER',
+        created=time()
     )
     u.password = '055071c9b1c48946ceae'
     return u
@@ -19,7 +21,8 @@ async def dummy_admin():
         id='4a3ceb040a3d81ead4af',
         username='aa4a3863d8a09867a53c',
         password_hash=ARGON.hash('530b095674764823ace8'),
-        role='ADMIN'
+        role='ADMIN',
+        created=time()
     )
     u.password = '530b095674764823ace8'
     return u
