7c17ff9d23f7352cb09f1eb99e5e5849139b257e
Sean Aye <hello@seanaye.ca>
2020-04-01 13:37:34 -0400

    fix object

--- ariadne_server/resolvers/types/balance_payload.py
+++ ariadne_server/resolvers/types/balance_payload.py
@@ -17,7 +17,7 @@ async def r_channel(*_):
 @BALANCE_PAYLOAD.field('liquidity')
 async def r_liquidity(*_):
     req = ln.ListChannelsRequest(public_only=True)
-    channels = await LND.stub.ListChannels(req)
+    channels = (await LND.stub.ListChannels(req)).channels
     return {
         'inbound': sum(x.remote_balance - x.remote_chan_reserve_sat for x in channels),
         'outbound': sum(x.local_balance - x.local_chan_reserve_sat for x in channels)
