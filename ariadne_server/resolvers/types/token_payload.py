d3c8236eb41b34ed0418316b7ff77e8e4ddbbc21
Sean Aye <hello@seanaye.ca>
2020-04-02 15:40:40 -0400

    encode jwt

--- ariadne_server/resolvers/types/token_payload.py
+++ ariadne_server/resolvers/types/token_payload.py
@@ -1,3 +1,4 @@
+import codecs
 from time import time
 from ariadne import ObjectType
 from classes.user import User
@@ -9,7 +10,7 @@ TOKEN_PAYLOAD = ObjectType('TokenPayload')
 @TOKEN_PAYLOAD.field('access')
 def r_access_token(user: User, *_) -> str:
     access_json = {
-        'id': user.id,
+        'id': codecs.encode(user.id, 'base-64'),
         'role': user.role,
         'iat': time(),
         'exp': time() + 900 # 15 minutes in seconds
@@ -20,7 +21,7 @@ def r_access_token(user: User, *_) -> str:
 @TOKEN_PAYLOAD.field('refresh')
 def r_refresh_token(user: User, *_) -> str:
     refresh_json = {
-        'id': user.id,
+        'id': codecs.encode(user.id, 'base-64'),
         'iat': time(),
         'exp': time() + 604800 # 1 week in seconds
     }
