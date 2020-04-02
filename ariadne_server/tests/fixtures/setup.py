62870c185763a9e493f1664842790cc24850a7a1
Sean Aye <hello@seanaye.ca>
2020-04-02 14:04:06 -0400

    chagensetup

--- ariadne_server/tests/fixtures/setup.py
+++ ariadne_server/tests/fixtures/setup.py
@@ -3,15 +3,9 @@ import pytest
 from context import GINO, REDIS, LND, PUBSUB
 from resolvers.schema import SCHEMA
 
-@pytest.yield_fixture(autouse=True, scope='session')
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
 
-
-@pytest.yield_fixture(autouse=True, scope='session')
-async def schema(event_loop):
+@pytest.fixture(autouse=True, scope='session')
+async def schema():
     await GINO.initialize()
     await REDIS.initialize()
     await LND.initialize()
