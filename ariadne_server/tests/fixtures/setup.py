e308a00c3710113f4acc14c89f804418e7fd0ff2
Sean Aye <hello@seanaye.ca>
2020-04-02 16:46:17 -0400

    use default event loop

--- ariadne_server/tests/fixtures/setup.py
+++ ariadne_server/tests/fixtures/setup.py
@@ -3,14 +3,8 @@ import pytest
 from context import GINO, REDIS, LND
 from resolvers.schema import SCHEMA
 
-@pytest.yield_fixture(autouse=True, scope='session')
-def event_loop(request):
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
 
-
-@pytest.yield_fixture(autouse=True, scope='session')
+@pytest.fixture(autouse=True, scope='session')
 async def schema(event_loop):
     await GINO.initialize()
     await REDIS.initialize()
