44b292c5880d1d90447f9ef630db6dd9e30cd268
Sean Aye <hello@seanaye.ca>
2020-04-02 14:41:11 -0400

    change event loop usage

--- ariadne_server/app.py
+++ ariadne_server/app.py
@@ -1,14 +1,14 @@
 """define app entry point"""
 from starlette.applications import Starlette
 from ariadne.asgi import GraphQL
-from context import LND, REDIS, GINO
+from context import LND, REDIS, GINO, PUBSUB
 from resolvers.schema import SCHEMA
 
 
 APP = Starlette(
     debug=True,
-    on_startup=[LND.initialize, REDIS.initialize, GINO.initialize],
-    on_shutdown=[LND.destroy, REDIS.destroy, GINO.destroy]
+    on_startup=[LND.initialize, REDIS.initialize, GINO.initialize, PUBSUB.initialize],
+    on_shutdown=[LND.destroy, REDIS.destroy, GINO.destroy, PUBSUB.destroy]
 )
 
 APP.mount('/graphql', GraphQL(SCHEMA, debug=True))
--- ariadne_server/tests/fixtures/setup.py
+++ ariadne_server/tests/fixtures/setup.py
@@ -3,7 +3,7 @@ import pytest
 from context import GINO, REDIS, LND, PUBSUB
 from resolvers.schema import SCHEMA
 
-@pytest.yield_fixture(autouse=True, scope='session')
+@pytest.yield_fixture(scope='session')
 def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
@@ -11,7 +11,7 @@ def event_loop():
 
 
 @pytest.yield_fixture(autouse=True, scope='session')
-async def schema(event_loop):
+async def schema():
     await GINO.initialize()
     await REDIS.initialize()
     await LND.initialize()
