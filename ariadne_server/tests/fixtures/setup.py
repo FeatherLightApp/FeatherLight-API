96ce12504f7e3a90523febe4ee928d8f902ed681
Sean Aye <hello@seanaye.ca>
2020-04-02 16:16:32 -0400

    change setup

--- ariadne_server/tests/fixtures/setup.py
+++ ariadne_server/tests/fixtures/setup.py
@@ -1,10 +1,10 @@
 import asyncio
 import pytest
-from context import GINO, REDIS, LND, PUBSUB
+from context import GINO, REDIS, LND
 from resolvers.schema import SCHEMA
 
-@pytest.yield_fixture(autouse=True, scope='session')
-def event_loop():
+@pytest.yield_fixture(scope='session')
+def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
