3792b46574a947ee8171b8dd17b20ab2016ca669
Sean Aye <hello@seanaye.ca>
2020-04-02 16:48:05 -0400

    scope event loop

--- ariadne_server/tests/fixtures/setup.py
+++ ariadne_server/tests/fixtures/setup.py
@@ -4,6 +4,10 @@ from context import GINO, REDIS, LND
 from resolvers.schema import SCHEMA
 
 
+@pytest.fixture(scope='session')
+def event_loop():
+    return asyncio.get_event_loop()
+
 @pytest.fixture(autouse=True, scope='session')
 async def schema(event_loop):
     await GINO.initialize()
