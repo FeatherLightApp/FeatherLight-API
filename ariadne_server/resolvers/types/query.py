a510f1b565f7d249a34f4ef9d6791e1f042e8151
Sean Aye <hello@seanaye.ca>
2020-05-08 21:58:00 -0400

    remove generic rpc command

--- ariadne_server/resolvers/types/query.py
+++ ariadne_server/resolvers/types/query.py
@@ -24,37 +24,6 @@ async def r_info(*_) -> dict:
 def r_passthrough(user: User, *_):
     return user
 
-# @QUERY.field('txs')
-# #@authenticate
-# async def r_txs(_: None, info, *, user: User) -> dict:
-#     address = await user.btc_address()
-#     await user.account_for_possible_txids()
-#     txs = await user.get_txs()
-#     locked_payments = await user.get_locked_payments()
-#     for locked in locked_payments:
-#         txs.append({
-#             'type': 'paid_invoice',
-#             'fee': floor(locked['amount'] * 0.01),
-#             'value': locked['amount'] + floor(locked['amount'] * 0.01),
-#             'timestamp': locked['timestamp'],
-#             'memo': 'Payment in transition'
-#         })
-#     return txs
-
-
-# @QUERY.field('invoices')
-# #@authenticate
-# async def r_invoices(_: None, info, *, last: Optional[int], user: User):
-#     invoices = await user.get_user_invoices()
-#     return invoices[-1 * last]
-
-# @QUERY.field('pending')
-# #@authenticate
-# async def r_pending(obj, info, **kwargs):
-#     address = await user.get_address()
-#     await user.account_for_possible_txids()
-#     return await user.get_pending_txs()
-
 
 @QUERY.field('decodeInvoice')
 async def r_decode_invoice(*_, invoice: str):
@@ -64,7 +33,6 @@ async def r_decode_invoice(*_, invoice: str):
     except GRPCError:
         return None
 
-
 # @QUERY.field('peers')
 # async def r_get_peers(_: None, info) -> dict:
 #     res = await info.context.bitcoind.req('getpeerinfo')
@@ -72,10 +40,3 @@ async def r_decode_invoice(*_, invoice: str):
 #         'ok': True,
 #         'peer_info': res.get('result') or []
 #     }
-
-# TODO remove temp query for generic rpc
-@QUERY.field('genericRPC')
-async def r_rpc_call(*_, command: str, params: str = '') -> str:
-    param_dict = None if not params else ast.literal_eval(params)
-    res = await BITCOIND.req(command, params=param_dict)
-    return json.dumps(res)
