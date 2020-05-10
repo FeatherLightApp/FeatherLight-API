3563ed56917143cb0410da2dec0da1433a991604
Sean Aye <hello@seanaye.ca>
2020-05-09 21:17:51 -0400

    import bake macaroon mutation

--- ariadne_server/resolvers/mutation/__init__.py
+++ ariadne_server/resolvers/mutation/__init__.py
@@ -1,17 +1,17 @@
-from .root import _MUTATION
 from .create_user import MUTATION as create_user
 from .bypass import MUTATION as bypass
 from .login import MUTATION as login
 from .logout import MUTATION as logout
 from .add_invoice import MUTATION as add_invoice
 from .pay_invoice import MUTATION as pay_invoice
+from .bake_macaroon import MUTATION as bake_macaroon
 
 MUTATION = [
-    _MUTATION,
     create_user,
     bypass,
     login,
     logout,
     add_invoice,
-    pay_invoice
+    pay_invoice,
+    bake_macaroon
 ]
