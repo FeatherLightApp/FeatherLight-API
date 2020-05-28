494e68c5500b4d66865d868633cd5cc1da4efb15
Sean Aye <hello@seanaye.ca>
2020-05-28 17:03:23 -0400

    import mutations

--- ariadne_server/resolvers/mutation/__init__.py
+++ ariadne_server/resolvers/mutation/__init__.py
@@ -5,6 +5,8 @@ from .logout import MUTATION as logout
 from .add_invoice import MUTATION as add_invoice
 from .pay_invoice import MUTATION as pay_invoice
 from .bake_macaroon import MUTATION as bake_macaroon
+from .prepay_wallet import MUTATION as prepay_wallet
+from .redeem_wallet import MUTATION as redeem_wallet
 
 MUTATION = [
     create_user,
@@ -13,5 +15,7 @@ MUTATION = [
     logout,
     add_invoice,
     pay_invoice,
-    bake_macaroon
+    bake_macaroon,
+    prepay_wallet,
+    redeem_wallet
 ]
